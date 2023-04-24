979a6c2782ab16392e0ded8627b9e0c591eefdfd
RazGG126 <118054621+RazGG126@users.noreply.github.com>
2023-04-24 18:52:53 +0900

    Added pool_pre_ping.

--- quote_web/data/db_session.py
+++ quote_web/data/db_session.py
@@ -20,7 +20,7 @@ def global_init(db_file):
     conn_str = f'sqlite:///{db_file.strip()}?check_same_thread=False'
     print(f"Подключение к базе данных по адресу {conn_str}")
 
-    engine = sa.create_engine(conn_str, echo=False)
+    engine = sa.create_engine(conn_str, echo=False, pool_pre_ping=True)
     __factory = orm.sessionmaker(bind=engine)
 
     from . import __all_models
